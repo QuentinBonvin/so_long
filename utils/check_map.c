d8a9c401b9247e6d9711ebfae0b0e8a559b7baa6
QuentinBonvin <qbonvin@student.42lausanne.ch>
2022-04-25 09:18:18 +0200

    final commit

--- utils/check_map.c
+++ utils/check_map.c
@@ -6,7 +6,7 @@
 /*   By: qbonvin <qbonvin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/04 15:05:33 by qbonvin           #+#    #+#             */
-/*   Updated: 2022/04/21 09:38:31 by qbonvin          ###   ########.fr       */
+/*   Updated: 2022/04/21 17:03:45 by qbonvin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,6 +45,7 @@ void	check_last_line(char **map, char *line, int width)
 
 	last_char = ft_strlen(line) - 1;
 	check_border_left_and_right(map, line, width);
+	//check_border_top_and_bot(map, width);
 	if (line[last_char] != '\n')
 		last_char += 1;
 	if (width != last_char)
@@ -75,20 +76,42 @@ void	check_border_left_and_right(char **map, char *line, int width)
 	}
 }
 
+void	check_border_top_and_bot(char **map, int width)
+{
+	int		i;
+	int		j;
+
+	i = -1;
+	j = ft_strlen(*map) - 1;
+	while (++i < width)
+	{
+		if ((*map)[i] != '1' || (*map)[j--] != '1')
+		{
+			error("Error\nTest top and bot\n");
+		}
+	}
+}
+
 /*******
  * check if on the map there is a '0' '1' 'C' 'E' 'P'
  *******/
 int	check_char_on_map(char **map)
 {
 	int	i;
+	int	player;
 
 	i = 0;
+	player = 0;
 	while ((*map)[i])
 	{
 		if ((*map)[i] != 'E' && (*map)[i] != 'P' &&
 			(*map)[i] != 'C' && (*map)[i] != '1' &&
 			(*map)[i] != '0' && (*map)[i] != '\n')
 			error("error\ninvalid char on the map\n");
+		else if ((*map)[i] == 'P')
+			player++;
+			if (player > 1)
+				error("error\nthere is not only 1 player");
 		i++;
 	}
 	return (1);
